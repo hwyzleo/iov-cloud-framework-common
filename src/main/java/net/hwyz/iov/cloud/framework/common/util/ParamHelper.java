18b1d84599b65c2bfafd60af52fead74da4071d7
hwyz_leo <hwyz.leo.ye@gmail.com>
2024-12-01 00:06:48 +0800

    改进: 参数辅助类增加模糊查询参数

--- src/main/java/net/hwyz/iov/cloud/framework/common/util/ParamHelper.java
+++ src/main/java/net/hwyz/iov/cloud/framework/common/util/ParamHelper.java
@@ -3,6 +3,7 @@ package net.hwyz.iov.cloud.framework.common.util;
 import cn.hutool.core.util.StrUtil;
 import net.hwyz.iov.cloud.framework.common.bean.ClientAccount;
 import net.hwyz.iov.cloud.framework.common.bean.MptAccount;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * 参数辅助类
@@ -35,6 +36,19 @@ public class ParamHelper {
             return null;
         }
         return StrUtil.emptyIfNull(mptAccount.getUsername()) + ":" + StrUtil.emptyIfNull(mptAccount.getAccountId());
+    }
+
+    /**
+     * 模糊查询参数
+     *
+     * @param param 参数
+     * @return 处理过的参数
+     */
+    public static String fuzzyQueryParam(String param) {
+        if (StringUtils.isBlank(param)) {
+            return null;
+        }
+        return "%" + param.trim() + "%";
     }
 
 }
