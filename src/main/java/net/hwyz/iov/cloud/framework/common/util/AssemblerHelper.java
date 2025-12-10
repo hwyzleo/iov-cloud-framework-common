8030b5a278e435db997372043e3052d649c6f790
hwyz_leo <hwyz.leo.ye@gmail.com>
2025-12-10 17:43:35 +0800

    功能: 添加JSON字符串与JSON对象转换功能

--- src/main/java/net/hwyz/iov/cloud/framework/common/util/AssemblerHelper.java
+++ src/main/java/net/hwyz/iov/cloud/framework/common/util/AssemblerHelper.java
@@ -1,6 +1,7 @@
 package net.hwyz.iov.cloud.framework.common.util;
 
 import cn.hutool.core.lang.TypeReference;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 
 import java.util.Map;
@@ -31,6 +32,26 @@ public class AssemblerHelper {
      */
     public static String map2Json(Map<String, Object> map) {
         return JSONUtil.toJsonStr(map);
+    }
+
+    /**
+     * JSON字符串转JSON对象
+     *
+     * @param jsonStr JSON字符串
+     * @return JSON对象
+     */
+    public static JSONObject string2Json(String jsonStr) {
+        return JSONUtil.parseObj(jsonStr);
+    }
+
+    /**
+     * JSON对象转JSON字符串
+     *
+     * @param json JSON对象
+     * @return JSON字符串
+     */
+    public static String json2String(JSONObject json) {
+        return json.toString();
     }
 
 }
