3ca269fe3aa21ed41ff05074fd3917715c701d40
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-04-17 22:47:23 +0800

    重构: 将自定义Headers从枚举重构为常量类

---
+++ src/main/java/net/hwyz/iov/cloud/framework/common/constant/CustomHeaders.java
@@ -0,0 +1,53 @@
+package net.hwyz.iov.cloud.framework.common.constant;
+
+/**
+ * 自定义Headers
+ *
+ * @author hwyz_leo
+ */
+public class CustomHeaders {
+    /**
+     * 认证信息
+     */
+    public static final String AUTHORIZATION = "Authorization";
+
+    /**
+     * 内容类型
+     */
+    public static final String CONTENT_TYPE = "Content-Type";
+
+    /**
+     * 客户端ID
+     */
+    public static final String CLIENT_ID = "X-Client-Id";
+
+    /**
+     * 客户端类型
+     */
+    public static final String CLIENT_TYPE = "X-Client-Type";
+
+    /**
+     * 设备ID
+     */
+    public static final String DEVICE_ID = "X-Device-Id";
+
+    /**
+     * 平台
+     */
+    public static final String PLATFORM = "X-Platform";
+
+    /**
+     * 操作系统版本
+     */
+    public static final String OS_VERSION = "X-Os-Version";
+
+    /**
+     * APP版本
+     */
+    public static final String APP_VERSION = "X-App-Version";
+
+    /**
+     * 车架号
+     */
+    public static final String VIN = "X-Vin";
+}
